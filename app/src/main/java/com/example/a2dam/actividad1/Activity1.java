e4ddb8e0fba3adb71cbcae0678f1f8e8c18b0ac6
ricardoborrull <ricardoborrull>
2017-10-05 20:22:01 +0200

    Acabar

--- app/src/main/java/com/example/a2dam/actividad1/Activity1.java
+++ app/src/main/java/com/example/a2dam/actividad1/Activity1.java
@@ -7,13 +7,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity1 extends AppCompatActivity {
 
     private EditText edNom;
     private EditText edApe;
     private Button botonMI;
+    private TextView msg;
+    private RadioButton hombre;
+    private RadioButton mujer;
     private final String TAG = "prueba";
+    final int SUBACT_1 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,9 @@ public class Activity1 extends AppCompatActivity {
         botonMI = (Button) findViewById(R.id.botonMI);
         edNom = (EditText) findViewById(R.id.edNom);
         edApe = (EditText) findViewById(R.id.edApe);
+        hombre = (RadioButton) findViewById(R.id.hombre);
+        mujer = (RadioButton) findViewById(R.id.mujer);
+        msg = (TextView) findViewById(R.id.msg);
 
         botonMI.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +40,30 @@ public class Activity1 extends AppCompatActivity {
                 Log.d(TAG, "Nombre: "+edNom.getText()+"\nApellidos: "+edApe.getText());
                 Intent i = new Intent(getApplicationContext(), Activity2.class);
                 i.putExtra("Nombre", edNom.getText());
-                startActivity(i);
+                i.putExtra("Apellido", edApe.getText());
+                if(edNom == null){
+                   if (edApe == null){
+                      if (hombre == null && mujer == null){
+
+                      }
+                   }
+                }
+
+                startActivityForResult(i, SUBACT_1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SUBACT_1) {
+            if(resultCode==RESULT_OK) {
+                msg.setText("Tienes "+data.getCharSequenceExtra("Edad")+" años");
+                Toast.makeText (this, "El subactivity ha continuado con éxito", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "El subactivity se ha cancelado", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/a2dam/actividad1/Activity2.java
+++ app/src/main/java/com/example/a2dam/actividad1/Activity2.java
@@ -3,15 +3,49 @@ package com.example.a2dam.actividad1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+
+    private Button btnCancel;
+    private Button btnNext;
+    private EditText edEdad;
+    private TextView Bienvenido;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        Intent i = getIntent();
-        String nombre = i.getStringExtra("Nombre");
+        Intent info = getIntent();
+        Bienvenido = (TextView) findViewById(R.id.Bienvenido);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        edEdad = (EditText) findViewById(R.id.edEdad);
+
+        Bienvenido.setText("Hola, "+info.getCharSequenceExtra("Nombre")+" "+info.getCharSequenceExtra("Apellido")+".");
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent edad = new Intent();
+                edad.putExtra("Edad", edEdad.getText());
+                setResult(RESULT_OK, edad);
+                finish();
+            }
+        });
+
     }
 }
