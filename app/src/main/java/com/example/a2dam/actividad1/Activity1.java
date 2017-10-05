1d8427ccd46b5ad9aae448c26581ebabf89011de
Ric <ricardoborrull97@gmail.com>
2017-10-05 23:19:59 +0200

    FAIL

--- app/src/main/java/com/example/a2dam/actividad1/Activity1.java
+++ app/src/main/java/com/example/a2dam/actividad1/Activity1.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,16 +38,21 @@ public class Activity1 extends AppCompatActivity {
         botonMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Nombre: "+edNom.getText()+"\nApellidos: "+edApe.getText());
+                Log.d(TAG, "Nombre: " + edNom.getText() + "\nApellidos: " + edApe.getText());
                 Intent i = new Intent(getApplicationContext(), Activity2.class);
                 i.putExtra("Nombre", edNom.getText());
                 i.putExtra("Apellido", edApe.getText());
-                if(edNom == null){
-                   if (edApe == null){
-                      if (hombre == null && mujer == null){
+                if (hombre.isSelected()) {
+                    i.putExtra("Sexo", hombre.getText());
+                }
+                if (mujer.isSelected()) {
+                    i.putExtra("Sexo", mujer.getText());
+                }
 
-                      }
-                   }
+                if (edNom == null) {
+                    if (edApe == null) {
+                        Toast.makeText(getApplicationContext(), "Introduce todos los datos para poder continuar.", Toast.LENGTH_LONG).show();
+                    }
                 }
 
                 startActivityForResult(i, SUBACT_1);
@@ -57,12 +63,31 @@ public class Activity1 extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SUBACT_1) {
-            if(resultCode==RESULT_OK) {
-                msg.setText("Tienes "+data.getCharSequenceExtra("Edad")+" años");
-                Toast.makeText (this, "El subactivity ha continuado con éxito", Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(this, "El subactivity se ha cancelado", Toast.LENGTH_LONG).show();
+        if (requestCode == SUBACT_1) {
+            if (resultCode == RESULT_OK) {
+                botonMI.setEnabled(false);
+                hombre.setEnabled(false);
+                mujer.setEnabled(false);
+                edApe.setEnabled(false);
+                edNom.setEnabled(false);
+
+                int edad = Integer.parseInt(String.valueOf(data.getCharSequenceExtra("Edad")));
+                if (edad < 0) {
+                    msg.setText("La edad no es válida");
+                    if (edad > 0 && edad < 18) {
+                        msg.setText("Eres menor de edad");
+                        if (edad > 18 && edad < 35) {
+                            msg.setText("Ya eres mayor de edad");
+                            if (edad >= 35) {
+                                msg.setText("Ay, ay, ay...");
+                            }
+                        }
+                    }
+                    //msg.setText("Tienes " + data.getCharSequenceExtra("Edad") + " años");
+                    Toast.makeText(this, "¡COMPLETADO!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "CANCELADO", Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
